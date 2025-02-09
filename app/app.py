f29708198f448899602a3d30a2cb9995d52b2a00
Orlando Jimenez <orlando.jmz13@gmail.com>
2025-02-09 01:42:05 -0500

    Pruebas de dominio

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "¡Hola Estamos trabajando en el cotizador y pagina web Taller El Kairos..!"
+    return "¡Hola, Estamos trabajando en el cotizador y pagina web Taller El Kairos..!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
